fe8db16c4726e3ed088a4329731a534446348eed
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-04-11 15:26:27 -0400

    WIP, play with idea for hiding passwords in response; TODO explore a cleaner SQL option.

--- server/src/controllers/thread.controller.ts
+++ server/src/controllers/thread.controller.ts
@@ -4,6 +4,7 @@ import {
   ThreadRepository,
   MessageRepository,
 } from "../database/repositories/repository";
+import { Thread } from "../database/entities/thread.entity";
 
 export const createThread = async (req: Request, res: Response) =>
   await attemptRequest(req, res, async () => {
@@ -12,16 +13,45 @@ export const createThread = async (req: Request, res: Response) =>
 
 export const retrieveUsersThreads = async (req: Request, res: Response) =>
   await attemptRequest(req, res, async () => {
-    const { id } = req.params;
-    const threads = await ThreadRepository.find({
-      where: {
-        id: parseInt(id),
-      },
-    });
+    const user = req["user"];
+    const userThreads = await ThreadRepository.createQueryBuilder("thread")
+      .leftJoin(
+        "thread.participants",
+        "participant",
+        "thread_participant.userId = :userId",
+        {
+          userId: user.id,
+        }
+      )
+      .getMany();
+
+    const threadsWithParticipants = await Promise.all(
+      userThreads.map(
+        async (thread) =>
+          await ThreadRepository.findOne({
+            relations: {
+              participants: true,
+            },
+            where: {
+              id: thread.id,
+            },
+          })
+      )
+    );
+
     return res.json({
       message: "Users Thread Data",
-      threads: threads.map(({ participants }) =>
-        participants.map(({ password, ...data }) => data)
+      // threads: threadsWithParticipants,
+      threadsTest: threadsWithParticipants.map(
+        ({ participants, ...threads }) => {
+          let hiddenPasswords = participants.map(
+            ({ password, ...data }) => data
+          );
+          return {
+            ...threads,
+            participants: hiddenPasswords,
+          };
+        }
       ),
     });
   });
--- server/src/routes/thread.routes.ts
+++ server/src/routes/thread.routes.ts
@@ -3,10 +3,11 @@ import {
   retrieveUsersThreads,
   retrieveThreadMessages,
 } from "../controllers/thread.controller";
+import { isAuthenticated } from "../middleware/auth.middleware";
 
 const prefix = "thread";
 
 export const threadRoutes = (router: Router) => {
-  router.get(`/${prefix}s/:id`, retrieveUsersThreads);
+  router.get(`/${prefix}s/`, [isAuthenticated], retrieveUsersThreads);
   router.get(`/${prefix}/messages/:id`, retrieveThreadMessages);
 };
